705fd44e1f7fd08b9b5d547cc769d25976f3d96e
promahbubul <mahbublalam500@gmail.com>
2024-11-09 15:02:55 +0600

    update progressive status page

--- src/pages/ProgressStatus/index.jsx
+++ src/pages/ProgressStatus/index.jsx
@@ -4,13 +4,15 @@ const ProgressStatus = () => {
   const [ll, setll] = useState("");
   const handleFormSubmit = (e) => {};
   return (
-    <div>
-      <form onSubmit={handleFormSubmit} action="" className="">
-        <input type="file" name="photo" id="" />
-        <input type="submit" value="Submit" />
-      </form>
-
-      <img src={ll} alt="" />
+    <div className="h-full flex justify-center items-center">
+      <h1 className="text-center text-5xl font-extrabold text-orange-500 underline">
+        <img
+          // src="https://cdni.iconscout.com/illustration/premium/thumb/coming-soon-banner-illustration-download-in-svg-png-gif-file-formats--new-logo-advert-miscellaneous-illustrations-3373565.png"
+          src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhMAlxfgvbav_3oUzqAm4pH-nCtTxmJwSZwbkWB9gWmTslzgeGgfXYbbk2Plo8QhKJtBGRSUd1LXs1gHJMyVhfWRZf1levzuA5ELc5lNl-pb8nYJhHV1o6tMSCmySnlkiFQNrFGhvL_GSA/s1600/bangladesh_flag_on_world_globe.gif"
+          alt=""
+          className=""
+        />
+      </h1>
     </div>
   );
 };
